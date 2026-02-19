9d6f1e8a1cdb0bf73a804aeacb8fb7421c5ea4df
Arnav Taley <arnavtaley@gmail.com>
2026-02-19 19:45:10 +0530

    feat(home): add temporary redirect to github repo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+import { ExternalLink } from "lucide-react";
+import Link from "next/link";
+
 export default function Home() {
-  return <div>Homepage</div>;
+  return (
+    <div className="min-h-svh flex flex-col items-center justify-center">
+      <Link href="https://github.com/lokithor476/clubhouse">
+        <div className="flex items-center gap-2 text-2xl font-bold group cursor-pointer">
+          <div className="transition-colors">
+            <span>github/</span>
+            <span className="group-hover:text-blue-500 group-hover:underline">clubhouse</span>
+          </div>
+          <ExternalLink className="group-hover:scale-110 transition-transform" />
+        </div>
+      </Link>
+    </div>
+  );
 }
