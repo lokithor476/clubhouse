ab2a1f42ff882dfedb8e9316f0b4ee5dda133b2a
Arnav Taley <arnavtaley@gmail.com>
2026-02-22 17:47:31 +0530

    fix(format): resolve tailwind classes sort

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,19 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="min-h-svh flex flex-col items-center justify-center">
+    <div className="flex min-h-svh flex-col items-center justify-center">
       <Link
         href="https://github.com/lokithor476/clubhouse"
         target="_blank"
         rel="noopener noreferrer"
         aria-label="Visit clubhouse repository on GitHub (opens in new tab)"
       >
-        <div className="flex items-center gap-2 text-2xl font-bold group">
+        <div className="group flex items-center gap-2 font-bold text-2xl">
           <span>github/</span>
           <span className="group-hover:text-blue-500 group-hover:underline">
             clubhouse
           </span>
-          <ExternalLink className="group-hover:scale-110 transition-transform" />
+          <ExternalLink className="transition-transform group-hover:scale-110" />
         </div>
       </Link>
     </div>
