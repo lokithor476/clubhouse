3a78bc5d0cd75931f10683cba756da9756ac0a6e
Arnav Taley <arnavtaley@gmail.com>
2026-02-22 19:40:58 +0530

    chore: add hero section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from "@/components/theme-providers";
-import "./globals.css";
+import { cn } from "@/lib/utils";
+import "@/styles/globals.css";
 
 export default function RootLayout({
   children,
@@ -8,14 +9,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>
+      <body className={cn("flex min-h-svh flex-col antialiased")}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <main className="flex flex-1 flex-col">{children}</main>
         </ThemeProvider>
       </body>
     </html>
