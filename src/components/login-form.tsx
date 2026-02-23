7359e605e1242d723497bc89f21e9ae4fbfd73b9
Arnav Taley <arnavtaley@gmail.com>
2026-02-23 19:49:01 +0530

    chore: integrate loginWithOAuth action

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState } from "react";
 import { Icons } from "@/components/icons";
 import { Button } from "@/components/ui/button";
 import {
@@ -7,20 +10,31 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { loginWithOAuth } from "@/modules/user/actions";
 
 export function LoginForm() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleLogin = async () => {
+    setIsLoading(true);
+    await loginWithOAuth("google");
+  };
+
   return (
     <Card className="mx-auto w-full max-w-sm">
       <CardHeader>
         <CardTitle>Login in your account</CardTitle>
-        <CardDescription>
-          Enter your credentials to access your account
-        </CardDescription>
+        <CardDescription>Continue to your account with Google</CardDescription>
       </CardHeader>
       <CardContent>
-        <Button variant="outline" className="w-full">
+        <Button
+          variant="outline"
+          className="w-full cursor-pointer"
+          onClick={handleLogin}
+          disabled={isLoading}
+        >
           <Icons.google className="mr-2 h-4 w-4" />
-          Sign in with Google
+          <p>{isLoading ? "Signing in..." : "Sign in with Google"}</p>
         </Button>
       </CardContent>
     </Card>
