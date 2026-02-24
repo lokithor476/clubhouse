5e3c8c8a01dd4b1a145006fbc74a019a5bfb1b12
Arnav Taley <arnavtaley@gmail.com>
2026-02-24 09:53:44 +0530

    chore(login): add login page

--- src/app/(app)/(auth)/login/page.tsx
+++ src/app/(app)/(auth)/login/page.tsx
@@ -1,9 +1,24 @@
+import { MoveRight } from "lucide-react";
+import Link from "next/link";
 import { LoginForm } from "@/components/login-form";
 
 export default function LoginPage() {
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center space-y-6">
+      <div className="flex items-center space-x-4">
+        <h2 className="text-lg font-semibold">Return to clubhouse</h2>
+        <MoveRight className="size-6" />
+      </div>
       <LoginForm />
+      <div className="inline-flex items-center gap-1 text-sm">
+        <span>Don&apos;t have an account?</span>
+        <Link
+          className="text-blue-600 dark:text-blue-400 hover:underline"
+          href="/signup"
+        >
+          Sign up
+        </Link>
+      </div>
     </div>
   );
 }
